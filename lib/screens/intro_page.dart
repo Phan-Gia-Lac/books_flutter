d5558b965ca708d854300ed66c60a0602ba288ee
Phan-Gia-Lac <phangialac@gmail.com>
2026-05-27 21:11:36 +0700

    feat(ui): added rive animation

--- lib/screens/intro_page.dart
+++ lib/screens/intro_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
 import '../AppRoutes.dart';
 
-Future<void> navigateToLogin(BuildContext context) async {
-  await Future.delayed(const Duration(seconds: 3));
+// Animation is 8 second
+Future<void> _navigateToLogin(BuildContext context) async {
+  await Future.delayed(const Duration(seconds: 8));
   if (context.mounted) {
     Navigator.pushReplacementNamed(context, AppRoutes.login);
   }
@@ -14,48 +16,39 @@ class IntroPage extends StatefulWidget {
   @override
   State<IntroPage> createState() => _IntroPageState();
 }
+
 class _IntroPageState extends State<IntroPage> {
+  // Using the new native file loader factory syntax
+  late final fileLoader = FileLoader.fromAsset(
+    "assets/animations/new_intro.riv", 
+    riveFactory: Factory.rive,
+  );
+  
   @override
   void initState() {
     super.initState();
     WidgetsBinding.instance.addPostFrameCallback((_) {
-      navigateToLogin(context);
+      _navigateToLogin(context);
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center, // Centers content vertically
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Icon(
-                Icons.wb_sunny_outlined, 
-                size: 80, 
-                color: Colors.blue,
+      backgroundColor: const Color(0xFF1F1F1F),
+      body: RiveWidgetBuilder(
+        fileLoader: fileLoader,
+        builder: (context, state) => switch (state) {
+          RiveLoading() => const Center(child: CircularProgressIndicator()),
+          RiveFailed() => const Center(child: Text("Failed to load animation")),
+          RiveLoaded() => RiveWidget(
+              fit: Fit.cover,
+              controller: RiveWidgetController(
+                state.file,
+                stateMachineSelector: StateMachineSelector.byName("State Machine 1"),
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Text(
-                'Welcome to our app!',
-                style: TextStyle(
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushReplacementNamed(context, AppRoutes.login);
-              },
-              child: const Text('Get Started'),
-            )
-          ],
-        ),
+        },
       ),
     );
   }
